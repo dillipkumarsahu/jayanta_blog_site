f8d7c7785b22aa0f6838c7a23678f449ece3774c
Dillip Kumar Sahu <sahudillipkumar466@gmail.com>
2021-06-15 22:01:41 +0530

    update

--- admin/assets/index.js
+++ admin/assets/index.js
@@ -25,22 +25,68 @@ $(document).ready(function() {
             var sub_heading = doc.data().sub_heading;
             var image = doc.data().image;
             var content = doc.data().content;
-            var time = new Date(doc.data().time.seconds);
+            // var time = new Date(doc.data().time.seconds);
+            var id = doc.id;
 
             ui = `
-                <div class="list-group mt-3">
-                    <a href="#" class="list-group-item list-group-item-action active text-center">
+                <ul class="list-group mt-3">
+                    <li class="list-group-item list-group-item-action active text-center">
                         `+heading+`
-                    </a>
-                    <a href="#" class="list-group-item list-group-item-action disabled text-center">`+time+`</a>
-                    <a href="#" class="list-group-item list-group-item-action disabled text-center">`+sub_heading+`</a>
-                    <a href="#" class="list-group-item list-group-item-action disabled text-center"><img src="`+image+`" alt="blog image" class="w-50"></a>
-                    <a href="#" class="list-group-item list-group-item-action disabled text-justify">`+content+`</a>
-                </div>
+                    </li>
+                    <li class="list-group-item list-group-item-action text-center">`+sub_heading+`</li>
+                    <li class="list-group-item list-group-item-action text-center"><img src="`+image+`" alt="blog image" class="w-50"></li>
+                    <li class="list-group-item list-group-item-action text-justify">`+content+`</li>
+                    <li class="list-group-item list-group-item-action bg-danger delete" blog-id="`+id+`">
+                         <i class="fa fa-trash-o text-light" style="font-size:25px;"></i>
+                    </li>
+                    
+                </ul>
             `;
+            if(heading != undefined){
+                $("#heading-lists").append(ui);
+            }
 
-            $("#heading-lists").append(ui);
+
+            /////delete coding
+        var pre_val = "";
+        $(".delete").each(function(){
+            $(this).click(function(){
+                pre_val = $(this).attr("blog-id");
+                var ui = `
+                    <button class="btn btn-info" id="close">No</button>
+                    <button class="btn btn-danger" id="dlt-yes" blg-id="`+pre_val+`">Yes</button>
+                `;
+                $("#popup").removeClass("d-none");
+                $("#confirm-btn").html(ui);
+                $("#close").click(function(){
+                    $("#popup").addClass("d-none");
+                });
+                $("#dlt-yes").click(function(){
+                    $("#confirm-btn").html('<img src="./../assets/loader.gif" alt="loader" class="w-50">');
+                    var result = db.collection('blogs').doc(pre_val).delete();
+                    if(result)
+                    {
+                            var ui = `
+                                <h6 class="text-success">Blog Deleted !</h6>
+                            `;
+                            $("#confirm-btn").html(ui);
+                            
+                            $(".delete").each(function(){
+                                if($(this).attr("blog-id") == pre_val){
+                                    $(this).parent().remove();
+                                }
+                            });
+                            setTimeout(function(){
+                                $("#popup").addClass("d-none");
+                            },1000);
+                    }
+                });
+
+                
+            });
         });
+        });
+        
     });
 
 
@@ -64,17 +110,25 @@ $(document).ready(function() {
         var content = $("#content").val();
         var timestamp = firebase.firestore.FieldValue.serverTimestamp();
 
-        db.collection('blogs').add({
+        var result = db.collection('blogs').add({
             heading: heading,
             sub_heading: sub_heading,
             image:image_data,
             content:content,
             time:timestamp
         });
-        
+        // console.log(result);
+        if(result)
+        {   
+            setTimeout(function(){
+                $("#add-blog").trigger("reset");
+            },1000);
+        }
     });
 });
 
 
-
+$(document).ready(function(){
+    
+});
 
